3e76cb87cd02f9b4e3ac3700eca82656f512edad
Christian Couder <chriscool@tuxfamily.org>
2016-07-13 10:23:57 +0200

    sharness: add t0010-multihash-basics.sh

---
+++ tests/sharness/t0010-multihash-basics.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+test_description="Test multihash basics"
+
+. ./lib/sharness/sharness.sh
+
+test_expect_success "current dir is writable" '
+	echo "It works!" >test.txt
+'
+
+test_expect_success "multihash is available" '
+	type multihash
+'
+
+test_expect_success "'multihash --help' looks good" '
+	multihash --help >actual 2>&1 || true &&
+	egrep -i "usage" actual >/dev/null
+'
+
+for opt in algorithm check encoding length quiet help
+do 
+	test_expect_success "'multihash --help' mention -$opt option" '
+		egrep "[-]$opt" actual >/dev/null
+	'
+done
+
+test_expect_success "'multihash test.txt' works" '
+	multihash test.txt >actual
+'
+
+test_expect_success "'multihash test.txt' output looks good" '
+	echo "QmTwovvskpD1hzuJA8wLA73wjxSisrVknKeNvGZVyjDguU" >expected &&
+	test_cmp expected actual
+'
+
+test_done
+
+# vi: set ft=sh :
